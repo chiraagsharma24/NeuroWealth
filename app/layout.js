2b7704daa2543ec1098719459f011f5fe070a92b
Chirag Sharma <chiragnoida3345@gmail.com>
2025-03-25 04:18:30 +0530

    Added authentication from clerk

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,19 +20,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* header */}
-        {children}
-        {/* footer */}
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-          Made with ❤️ by <span className="font-semibold">Chirag Sharma</span>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {/* header */}
+          <Header></Header>
+          <main className="min-h-screen"> {children}</main>
+          {/* footer */}
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              Made with ❤️ by{" "}
+              <span className="font-semibold">Chirag Sharma</span>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
