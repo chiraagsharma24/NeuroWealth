5f65d0f39397c1a6d21c5f0aff16337733755e42
Chirag Sharma <chiragnoida3345@gmail.com>
2025-03-27 05:45:05 +0530

    Landing Page Complete

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
+import { Inter } from "next/font/google";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +30,11 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {/* header */}
           <Header></Header>
-          <main className="min-h-screen"> {children}</main>
+          <main className="min-h-screen pt-24"> {children}</main>
           {/* footer */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
--- app/page.js
+++ app/page.js
@@ -1,7 +1,115 @@
-import Image from "next/image";
+import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
+import Image from "next/image"; //1,5,21
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="my-5">
+      <HeroSection />
+
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+            {statsData.map((statsData, index) => (
+              <div key={index} className="text-center">
+                <div className="text-4xl font-bold text-blue-600 mb-2">
+                  {statsData.value}
+                </div>
+                <div className="text-gray-600">{statsData.label}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-12">Why Should you use Neuro-Wealth?</h2>
+          <div className="grid grid-cols-3 gap-5">
+            {featuresData.map((featuresData, index) => (
+              <Card key={index} className="p-6">
+                <CardContent className="space-y-4 pt-4">
+                  {featuresData.icon}
+                  <h3 className="text-xl font-semibold">{featuresData.title}</h3>
+                  <p className="text-gray-600">{featuresData.description}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-16">How It Works</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+            {howItWorksData.map((step, index) => (
+              <div key={index} className="text-center">
+                <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-6">
+                  {step.icon}
+                </div>
+                <h3 className="text-xl font-semibold mb-4">{step.title}</h3>
+                <p className="text-gray-600">{step.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section id="testimonials" className="py-20">
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-bold text-center mb-16">
+            What Our Users Say
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {testimonialsData.map((testimonial, index) => (
+              <Card key={index} className="p-6">
+                <CardContent className="pt-4">
+                  <div className="flex items-center mb-4">
+                    <Image
+                      src={"/logo"}
+                      alt={testimonial.name}
+                      width={40}
+                      height={40}
+                      className="rounded-full"
+                    />
+                    <div className="ml-4">
+                      <div className="font-semibold">{testimonial.name}</div>
+                      <div className="text-sm text-gray-600">
+                        {testimonial.role}
+                      </div>
+                    </div>
+                  </div>
+                  <p className="text-gray-600">{testimonial.quote}</p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blue-600">
+        <div className="container mx-auto px-4 text-center">
+          <h2 className="text-3xl font-bold text-white mb-4">
+            Ready to Take Control of Your Finances?
+          </h2>
+          <p className="text-blue-100 mb-8 max-w-2xl mx-auto">
+            Join hundreds of users who are already managing their finances
+            smarter with Neuro-Wealth
+          </p>
+          <Link href="/dashboard">
+            <Button
+              size="lg"
+              className="bg-white text-blue-600 hover:bg-blue-50 "
+            >
+              Join Today
+            </Button>
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 }
