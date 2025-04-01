94cc02d73d236616979d981319d220d9f4be527b
Chirag Sharma <chiragnoida3345@gmail.com>
2025-04-02 04:01:35 +0530

    Added /Dasboard endpoint

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function DashboardPage(){
+    return <div className="px-5">
+        {/* Budget Progress */}
+
+        {/* Overview  */}
+
+        {/* Accounts Grid */}
+    </div>
+}
