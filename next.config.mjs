d1b55f53a043c274db2d46e444fa0ed432e5f7d1
Chirag Sharma <chiragnoida3345@gmail.com>
2025-05-12 01:02:33 +0530

    Fixed Version mismatch

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
+  webpack: (config) => {
+    config.cache = {
+      type: "filesystem",
+      compression: "gzip",
+      cacheDirectory: path.resolve(__dirname, ".next/cache/webpack"),
+    };
+    return config;
   },
-
   experimental: {
     serverActions: {
       bodySizeLimit: "5mb",
